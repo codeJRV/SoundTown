7f3ce32b78a513201a2bae642cdbec3e72d1d301
Jibin Rajan Varghese <email.jrv@gmail.com>
2019-04-13 15:16:00 -0500

    API integrated

--- NowPlayingOnSpotify/config.js
+++ NowPlayingOnSpotify/config.js
@@ -1,5 +1,5 @@
 "use strict";
-var SPOTIFY_CLIENT_ID = '410511aa47884da3b3fd502fd8ab49a9';
+var SPOTIFY_CLIENT_ID = 'f2278a950d894804a405e74c7a357db2';
 var REMOTE_SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/NowPlayingOnSpotify/'
-var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
+var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost:8080/';
 var SPOTIFY_REDIRECT_URI = LOCAL_SPOTIFY_REDIRECT_URI;
